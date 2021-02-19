36bea3e885e5d6608d61735101a042a83a5ee578
Serggi0 <serve4mia@gmail.com>
2021-02-19 13:23:12 +0300

    Modify gendiff.py: only main function in file

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
+from gendiff import engine
 
-import argparse
 
-parser = argparse.ArgumentParser(description='Generate diff')
-parser.add_argument('first_file')
-parser.add_argument('second_file')
-parser.add_argument('-f', '--format')
+def main():
+    engine.gendiff()
 
-args = parser.parse_args()
-print(args.first_file)
+
+if __name__ == '__main__':
+    main()
